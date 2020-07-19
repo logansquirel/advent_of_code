6fed19b1f279773b7ebea07ca2ad9158e8b26a3d
Logan SQUIREL <logan.squirel@protonmail.com>
2020-07-19 22:24:19 +0200

    Add AoC 2016 - day 01

---
+++ aoc_2016/day_01/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::Read;
+
+use aoc_2016_day_01::puzzle_one;
+use aoc_2016_day_01::puzzle_two;
+use aoc_2016_day_01::Result;
+
+fn main() -> Result<()> {
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input)?;
+    println!("Advent of Code 2016-01");
+    println!("------ Puzzle 1 ------");
+    println!("{}", puzzle_one(&input));
+    println!();
+    println!("------ Puzzle 2 ------");
+    println!("{}", puzzle_two(&input));
+    println!();
+    Ok(())
+}

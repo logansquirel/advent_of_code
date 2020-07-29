d39f33780ec7d43831e037a26a4f9a9045041dbc
Logan SQUIREL <logan.squirel@protonmail.com>
2020-07-29 11:02:42 +0200

    Remove result from main(s)

--- aoc_2015/day_01/src/main.rs
+++ aoc_2015/day_01/src/main.rs
@@ -3,9 +3,11 @@ use std::io::Read;
 use aoc_2015_day_01::puzzle_one;
 use aoc_2015_day_01::puzzle_two;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() {
     let mut input = String::new();
-    std::io::stdin().read_to_string(&mut input)?;
+    std::io::stdin()
+        .read_to_string(&mut input)
+        .expect("failed to read input");
     println!("Advent of Code 2015-01");
     println!("------ Puzzle 1 ------");
     println!("{}", puzzle_one(&input));
@@ -13,5 +15,4 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("------ Puzzle 2 ------");
     println!("{}", puzzle_two(&input));
     println!();
-    Ok(())
 }

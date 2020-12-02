d0b5c0cc6b91c096803bb17aaa48cee5f7ff4c7a
Logan Squirel <logan.squirel@protonmail.com>
2020-12-02 09:35:56 +0100

    Update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,23 +1,9 @@
 #!/bin/bash
 set -e
+
+cargo fmt
+cargo check --workspace --all-targets --all-features
 cargo clean
-echo "compiling..."
-cargo check --quiet --workspace --all-targets --all-features
-echo "OK"
-echo "check format..."
-cargo fmt --all -- --check
-echo "OK"
-cargo clean
-echo "check lints..."
-cargo clippy --quiet --workspace --all-targets --all-features -- -D warnings
-echo "OK"
-cargo clean
-echo "building..."
-cargo build --quiet --release --workspace --all-targets --all-features
-echo "OK"
-echo "testing libraries..."
-cargo test --quiet --release --workspace --lib --all-features
-echo "OK"
-echo "testing solutions..."
-cargo test --quiet --release --workspace --test solution --all-features
-echo "OK"
+cargo clippy --workspace --all-targets --all-features
+cargo build --workspace --all-targets --all-features
+cargo test --workspace --all-targets --all-features

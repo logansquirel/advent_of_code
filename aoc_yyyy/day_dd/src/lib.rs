d462b78d07ef50c19fdd3c156d87ef58c311c69f
Logan Squirel <logan.squirel@protonmail.com>
2020-12-02 08:50:03 +0100

    Update template lib.rs

--- aoc_yyyy/day_dd/src/lib.rs
+++ aoc_yyyy/day_dd/src/lib.rs
@@ -1,22 +1,17 @@
 pub fn puzzle_one(input: &str) -> String {
-    String::from(&(input.trim())[..3])
+    String::from(input[..3].trim())
 }
 
 pub fn puzzle_two(input: &str) -> String {
-    String::from(&(input.trim())[3..])
+    String::from(input[3..].trim())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[test]
+fn puzzle_one_example() {
+    assert_eq!(String::from("foo"), puzzle_one("foobar"));
+}
 
-    #[test]
-    fn puzzle_one_example() {
-        assert_eq!(String::from("foo"), puzzle_one("foobar"));
-    }
-
-    #[test]
-    fn puzzle_two_example() {
-        assert_eq!(String::from("bar"), puzzle_two("foobar"));
-    }
+#[test]
+fn puzzle_two_example() {
+    assert_eq!(String::from("bar"), puzzle_two("foobar"));
 }

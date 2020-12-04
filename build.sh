990661e4aa38f53dd64437752985a38c1289aabb
Logan Squirel <logan.squirel@protonmail.com>
2020-12-04 16:39:14 +0100

    Update build script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 cargo fmt
 mdl -ag .
 cargo clean
-cargo clippy --workspace --all-targets --all-features
-cargo build --workspace --all-targets --all-features
-cargo test --workspace --all-targets --all-features
+cargo clippy
+cargo build
+cargo test
